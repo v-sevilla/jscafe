e3b3cf8cf22830fea9aa07b1cbe6e17bcb3185c9
v-sevilla <126656971+v-sevilla@users.noreply.github.com>
2024-03-28 21:10:48 -1000

    feat: Add english version of webpage

---
+++ js/english.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+    async function getHotDrinks() {
+        let url = "/drinks-api/hotDrinks.json";
+        try {
+            let res = await fetch(url);
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function getIcedDrinks() {
+        let url = "/drinks-api/icedDrinks.json";
+        try {
+            let res = await fetch(url);
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function renderHotDrinkDesc() {
+        let hotDrinks = await getHotDrinks();
+        let html = '';
+        hotDrinks.forEach(drink => {
+            let htmlSegment = `<section class="hotDrinks">
+                                <img src="${drink.image}" >
+                                <p>${drink.title}</p>
+                                <span class="drinkDescription">${drink.description}</span>
+                            </section>`;
+    
+            html += htmlSegment;
+        });
+    
+        let hotDrinksDesc = document.querySelector('.hotDrinkDesc');
+        hotDrinksDesc.innerHTML = html;
+    }
+    
+    renderHotDrinkDesc();
+
+    async function renderIcedDrinkDesc() {
+        let icedDrinks = await getIcedDrinks();
+        let html = '';
+        icedDrinks.forEach(drink => {
+            let htmlSegment = `<section class="icedDrinks">
+                                <img src="${drink.image}" >
+                                <p>${drink.title}</p>
+                                <span class="drinkDescription">${drink.description}</span>
+                            </section>`;
+    
+            html += htmlSegment;
+        });
+    
+        let icedDrinksDesc = document.querySelector('.icedDrinkDesc');
+        icedDrinksDesc.innerHTML = html;
+    }
+    
+    renderIcedDrinkDesc();
+});
