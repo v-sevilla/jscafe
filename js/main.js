9379caa4e6eb41ad3b8b984cab567e936d6d29de
v-sevilla <126656971+v-sevilla@users.noreply.github.com>
2024-03-28 18:37:10 -1000

    feat: Create menu and description pages

--- js/main.js
+++ js/main.js
@@ -10,22 +10,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     async function renderHotDrinks() {
-        let hotDrinks = await getHotDrinks();
-        let html = '';
-        hotDrinks.forEach(drink => {
-            let htmlSegment = `<section class="hotDrinks">
-                                <img src="${drink.image}" >
-                                <p>${drink.title}</p>
-                                <span class="drinkDescription">${drink.description}</span>
-                            </section>`;
-    
-            html += htmlSegment;
-        });
-    
-        let hotDrinksList = document.querySelector('.hotDrinkList');
-        hotDrinksList.innerHTML = html;
-    }
-    
+       let hotDrinks = await getHotDrinks();
+       let html = '';
+       hotDrinks.forEach(drink => {
+           let htmlSegment = `<section class="hotDrinks">
+                               <img src="${drink.image}" >
+                               <p>${drink.title}</p>
+                           </section>`;
+   
+           html += htmlSegment;
+       });
+   
+       let hotDrinksList = document.querySelector('.hotDrinkList');
+       hotDrinksList.innerHTML = html;
+   }
+   
     renderHotDrinks();
 
     async function getIcedDrinks() {
@@ -37,7 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log(error);
         }
     }
-
     async function renderIcedDrinks() {
         let icedDrinks = await getIcedDrinks();
         let html = '';
@@ -56,5 +54,41 @@ document.addEventListener("DOMContentLoaded", () => {
     
     renderIcedDrinks();
 
+    async function renderHotDrinkDesc() {
+        let hotDrinks = await getHotDrinks();
+        let html = '';
+        hotDrinks.forEach(drink => {
+            let htmlSegment = `<section class="hotDrinks">
+                                <img src="${drink.image}" >
+                                <p>${drink.title}</p>
+                                <span class="drinkDescription">${drink.description}</span>
+                            </section>`;
+    
+            html += htmlSegment;
+        });
+    
+        let hotDrinksDesc = document.querySelector('.hotDrinkDesc');
+        hotDrinksDesc.innerHTML = html;
+    }
+    
+    renderHotDrinkDesc();
 
+    async function renderIcedDrinkDesc() {
+        let icedDrinks = await getIcedDrinks();
+        let html = '';
+        icedDrinks.forEach(drink => {
+            let htmlSegment = `<section class="icedDrinks">
+                                <img src="${drink.image}" >
+                                <p>${drink.title}</p>
+                                <span class="drinkDescription">${drink.description}</span>
+                            </section>`;
+    
+            html += htmlSegment;
+        });
+    
+        let icedDrinksDesc = document.querySelector('.icedDrinkDesc');
+        icedDrinksDesc.innerHTML = html;
+    }
+    
+    renderIcedDrinkDesc();
 });
